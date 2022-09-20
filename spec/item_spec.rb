3c9cf3a14fcabca50085408b4844c05ff2ba9f74
Amanda Ross <amikaross@gmail.com>
2022-09-20 09:10:12 -0600

    Create item and vendor class and test files and add tests to item_spec

---
+++ spec/item_spec.rb
@@ -0,0 +1,18 @@
+require "rspec"
+require "./lib/item"
+
+RSpec.describe Item do 
+  before(:each) do 
+    @item1 = Item.new({name: "Peach", price: "$0.75"})
+  end
+  describe "#initialize" do 
+    it "exists" do 
+      expect(@item1).to be_an_instance_of(Item)
+    end
+
+    it "has readable name and price attributes" do 
+      expect(@item1.name).to eq("Peach")
+      expect(@item1.price).to eq(0.75)
+    end
+  end
+end
